c30dc5273f8ac6425a9caa062d1defd1568a102e
GeorgeM101 <george.mboya@student.moringaschool.com>
2021-11-14 11:54:14 +0300

    dates calculation

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-var dates = document.getElementById("dates").value;
-var gender = document.getElementById("Gender").value;
+var dates = document.getElementById("dates");
+var gender = document.getElementById("Gender");
 
-function formInput(){
-    if(dates.trim() === "" || gender.trim() === ""){
-        alert("Fill all inputs");
+function formInput() {
+    if (dates.value === "" || Gender.value === ""){
+    alert("Input all fields");
+    return false;
+    }else {
+        var dateArray = dates.value.split('-');
+        var century = parseInt((dateArray[0]/100))+1;
+        alert(century);
+        var calcDates= ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7;
         return false;
-    }
-
+    }   
+    
 }
+
